392f30e14b705222f35ef1ce0ec01fb61313ec00
Muhodari Sage <sagemuho@gmail.com>
2021-08-14 11:15:57 +0200

    read to start

---
+++ src/app/Components/Header/Header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './Header.component.html',
+  styleUrls: ['./Header.component.css']
+})
+
+export class HeaderComponent {
+  title = 'myPortfolio';
+}
